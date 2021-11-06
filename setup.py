a95f83cf305861f0f1c5365c6d78807506c7d4eb
SirLich <lrkoehler77@gmail.com>
2021-11-06 11:33:33 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = open(
 setup(
   name = 'reticulator',
   packages = ['reticulator'],
-  version = 'v0.0.1-beta',
+  version = 'v0.0.2-beta',
   license='MIT',
   description = 'Reticulator is a pack-access library for Minecraft Bedrock Addons.',
   author = 'SirLich',
