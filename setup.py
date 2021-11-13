7f9903508e62be27212ebd097acec034293d81ce
SirLich <lrkoehler77@gmail.com>
2021-11-13 14:17:50 +0100

    Add additional tests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     'dpath',
   ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Libraries',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
--- test/test.py
+++ test/test.py
@@ -1,13 +1,90 @@
 import unittest
 import sys
 
-
-
-
-
-
 sys.path.insert(0, '../reticulator')
 from reticulator import *
+
+def get_packs():
+    project = Project('./content/bp/', './content/rp/')
+    return project.behavior_pack, project.resource_pack
+
+def create_output_directory():
+    os.mkdir('test_output')
+
+def clean_up_output_directory():
+    os.remove('test_output')
+
+class TestDirty(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bp, self.rp = get_packs()
+        self.entity = self.bp.get_entity('minecraft:dolphin')
+
+    def test_property(self):
+        self.assertEqual(self.entity.dirty, False)
+        self.entity.identifier = 'bob'
+        self.assertEqual(self.entity.dirty, True)
+
+    def test_jsonpath(self):
+        self.assertEqual(self.entity.dirty, False)
+        self.entity.set_jsonpath('new_key', {})
+        self.assertEqual(self.entity.dirty, True)
+
+    def test_data(self):
+        self.assertEqual(self.entity.dirty, False)
+        self.entity.data['new_key'] = []
+        self.assertEqual(self.entity.dirty, True)
+
+    def test_subresource(self):
+        component = self.entity.get_component('minecraft:type_family')
+        self.assertEqual(self.entity.dirty, False)
+        self.assertEqual(component.dirty, False)
+
+        component.set_jsonpath('new_key', "")
+
+        self.assertEqual(self.entity.dirty, True)
+        self.assertEqual(component.dirty, True)
+
+    def test_subresource_id(self):
+        component = self.entity.get_component('minecraft:type_family')
+        self.assertEqual(self.entity.dirty, False)
+        self.assertEqual(component.dirty, False)
+
+        component.id = 'new_component_name'
+
+        self.assertEqual(self.entity.dirty, True)
+        self.assertEqual(component.dirty, True)
+
+class TestParticle(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bp, self.rp = get_packs()
+
+    def test_particle_properties(self):
+        """
+        Checks that the properties of the test particle is correct.
+        """
+        particle = self.rp.get_particle('minecraft:death_explosion_emitter')
+        self.assertEqual(particle.identifier, 'minecraft:death_explosion_emitter')
+        self.assertEqual(particle.format_version, "1.10.0")
+
+    def test_particle_path(self):
+        """
+        Checks whether the FileResource properties are correct.
+        """
+        self.assertEqual(len(self.rp.particles), 2)
+        particle = self.rp.get_particle('minecraft:death_explosion_emitter')
+        self.assertEqual(particle.file_name, 'explosion_death.json')
+        self.assertEqual(particle.file_path, 'particles\\explosions\\explosion_death.json')
+
+    def test_particle_subresources(self):
+        """
+        Tests access into the 'components' property of the particle.
+        """ 
+        particle = self.rp.get_particle('minecraft:death_explosion_emitter')
+        self.assertEqual(len(particle.components), 10)
+        component = particle.get_component('minecraft:emitter_rate_instant')
+        self.assertEqual(component.id, 'minecraft:emitter_rate_instant')
+        self.assertEqual(component.get_jsonpath('num_particles'), 20)
+        self.assertEqual(component.data['num_particles'], 20)
 
 class TestJsonPathAccess(unittest.TestCase):
     """
@@ -45,10 +122,6 @@ class TestJsonPathAccess(unittest.TestCase):
         # Test jsonpath which does not exist
         with self.assertRaises(AssetNotFoundError):
             dolphin.get_jsonpath('dne')
-
-    
-
-
 
 if __name__ == '__main__':
     unittest.main()
