9a851b6f5070b6a8e201dd092967c89fd2ebf302
SirLich <lrkoehler77@gmail.com>
2022-01-05 16:16:53 +0100

    Git push

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = open(
 setup(
   name = 'reticulator',
   packages = ['reticulator'],
-  version = 'v0.0.7-beta',
+  version = 'v0.0.8-beta',
   license='MIT',
   description = 'Reticulator is a pack-access library for Minecraft Bedrock Addons.',
   author = 'SirLich',
